ae0526643d02c233ead557f483b3a85dc3412a8c
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-07 22:41:14 -0300

    buscas com múltiplos critérios

--- src/main/java/br/com/fiap/semweb/principal/Principal.java
+++ src/main/java/br/com/fiap/semweb/principal/Principal.java
@@ -43,7 +43,8 @@ public class Principal {
                     5 - Buscar séries por ator   
                     6 - Top 5 séries  
                     7 - Buscar séries por categoria  
-                    8 - Filtrar séries         
+                    8 - Filtrar séries      
+                    9 - Buscar episódios por trecho   
                     0 - Sair                                 
                     """;
 
@@ -75,6 +76,9 @@ public class Principal {
                     break;
                 case 8:
                     filtrarSeriesPorTemporadaEAvaliacao();
+                    break;
+                case 9:
+                    buscarEpisodioPorTrecho();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -186,5 +190,14 @@ public class Principal {
         System.out.println("*** Séries filtradas ***");
         filtroSeries.forEach(s ->
                 System.out.println(s.getTitulo() + "  - avaliação: " + s.getAvaliacao()));
+    }
+    private void buscarEpisodioPorTrecho() {
+        System.out.println("Qual o nome do episódio para busca?");
+        var trechoEpisodio = leitura.nextLine();
+        List<Episodio> episodiosEncontrados = repositorio.episodiosPorTrecho(trechoEpisodio);
+        episodiosEncontrados.forEach(e ->
+                System.out.printf("Série: %s Temporada %s - Episódio %s - %s\n",
+                        e.getSerie().getTitulo(), e.getTemporada(),
+                        e.getNumeroEpisodio(), e.getTitulo()));
     }
 }
--- src/main/java/br/com/fiap/semweb/repository/SerieRepository.java
+++ src/main/java/br/com/fiap/semweb/repository/SerieRepository.java
@@ -1,6 +1,7 @@
 package br.com.fiap.semweb.repository;
 
 import br.com.fiap.semweb.model.Categoria;
+import br.com.fiap.semweb.model.Episodio;
 import br.com.fiap.semweb.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -23,4 +24,7 @@ public interface SerieRepository extends JpaRepository <Serie, Long> {
 
     @Query("select s from Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
     List<Serie> seriesPorTemporadaEAvaliacao(int totalTemporadas, double avaliacao);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
 }
