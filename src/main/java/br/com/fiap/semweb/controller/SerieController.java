9334d1b13421956837837b2638a9e1717822de0e
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-12 23:59:07 -0300

    otimizando

--- src/main/java/br/com/fiap/semweb/controller/SerieController.java
+++ src/main/java/br/com/fiap/semweb/controller/SerieController.java
@@ -4,23 +4,25 @@ import br.com.fiap.semweb.dto.SerieDTO;
 import br.com.fiap.semweb.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries() {
         return service.obterTodasAsSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obterTop5Series() {
         return service.obterTop5Series();
     }
