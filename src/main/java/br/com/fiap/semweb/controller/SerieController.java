6deeb3701d8160be44023a0457c73b27d00f3eec
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-14 23:19:06 -0300

    Get por Id

--- src/main/java/br/com/fiap/semweb/controller/SerieController.java
+++ src/main/java/br/com/fiap/semweb/controller/SerieController.java
@@ -4,10 +4,7 @@ import br.com.fiap.semweb.dto.SerieDTO;
 import br.com.fiap.semweb.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +34,8 @@ public class SerieController {
         return service.obterLancamentos();
     }
 
-
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
+    }
 }
--- src/main/java/br/com/fiap/semweb/dto/SerieDTO.java
+++ src/main/java/br/com/fiap/semweb/dto/SerieDTO.java
@@ -3,6 +3,9 @@ package br.com.fiap.semweb.dto;
 import br.com.fiap.semweb.model.Categoria;
 
 public record SerieDTO(
+
+        Long id,
+
         String titulo,
 
         Integer totalTemporadas,
